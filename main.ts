3730bdbb12c6d31f2284c37df103e8e84f99e51a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-08-14 02:58:27 +0900

    feat: Standardize quotes and semicolons in main.ts
    This commit standardizes the use of double quotes for strings and
    ensures that all statements are properly terminated with semicolons
    in the main.ts file. This improves code readability and consistency
    across the codebase.

--- main.ts
+++ main.ts
@@ -1,56 +1,56 @@
 type Element = {
-  kind: 'text';
+  kind: "text";
   file: File;
   content: string;
 } | {
-  kind: 'head1';
+  kind: "head1";
   file: File;
   children: Element[];
-}
+};
 
 interface File {
   offsetStart: number;
   offsetEnd: number;
   path: string | undefined;
-  content: string
+  content: string;
 }
 
-type State = 'sentinel' | 'head1' | 'text';
+type State = "sentinel" | "head1" | "text";
 
 function text(file: File): Element {
   const content = file.content.substring(file.offsetStart, file.offsetEnd);
-  return { kind: 'text', file, content }
+  return { kind: "text", file, content };
 }
 
 function head1(file: File): Element {
-  const offsetStart = file.offsetStart + '=head1'.length + 1;
-  const children = parse({ ...file, offsetStart })
-  return { kind: 'head1', file, children }
+  const offsetStart = file.offsetStart + "=head1".length + 1;
+  const children = parse({ ...file, offsetStart });
+  return { kind: "head1", file, children };
 }
 
 export function parse(file: File): Element[] {
   const result: Element[] = [];
-  const stack: State[] = ['sentinel'];
+  const stack: State[] = ["sentinel"];
   let offsetStart = 0;
   let offsetEnd = 1;
-  for(const line of file.content.split('\n')) {
+  for (const line of file.content.split("\n")) {
     offsetEnd += line.length;
-    if (stack.at(-1) === 'head1') {
-      if (line.startsWith('\n')) {
+    if (stack.at(-1) === "head1") {
+      if (line.startsWith("\n")) {
         stack.pop();
-        result.push(head1({... file, offsetStart, offsetEnd}));
+        result.push(head1({ ...file, offsetStart, offsetEnd }));
       }
-    } else if (stack.at(-1) === 'text')  {
-      if (line.startsWith('\n')) {
+    } else if (stack.at(-1) === "text") {
+      if (line.startsWith("\n")) {
         stack.pop();
-        result.push(text({... file, offsetStart, offsetEnd}));
+        result.push(text({ ...file, offsetStart, offsetEnd }));
       }
-    } else if (line.startsWith('=head1')) {
+    } else if (line.startsWith("=head1")) {
       offsetStart = offsetEnd;
-      stack.push('head1');
+      stack.push("head1");
     } else {
       offsetStart = offsetEnd;
-      stack.push('text');
+      stack.push("text");
     }
   }
   return result;
